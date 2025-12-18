065403f8f97b8e59986ab180261044c71723dc31
CodeHarryson <hmanjuwo@outlook.com>
2025-12-18 14:28:00 -0600

    added SG

--- main.tf
+++ main.tf
@@ -5,8 +5,39 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-00e428798e77d38d9"
     instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p ${var.server_port} &
+        EOF
+    user_data_replace_on_change = true
+
 
     tags = {
         Name = "terraform-example"
     }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+variable "server_port" {
+    description = "Port of the server that will handle HTTP requests"
+    type = number
+    default = 8080
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "Public IP address of web server" 
 }
