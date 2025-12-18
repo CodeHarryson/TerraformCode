0a38293070125a1823710b832f9286028324a567
CodeHarryson <hmanjuwo@outlook.com>
2025-12-18 17:12:53 -0600

    Working on implementation of ALB

--- main.tf
+++ main.tf
@@ -2,21 +2,71 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-00e428798e77d38d9"
+resource "aws_launch_configuration " "example" {
+    image_id = "ami-00e428798e77d38d9"
     instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
         #!/bin/bash
         echo "Hello, World" > index.html
         nohup busybox httpd -f -p ${var.server_port} &
         EOF
-    user_data_replace_on_change = true
+    
+    lifecylce {
+        create_before_destroy = true
+    }
+}
 
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnets.default.ids
 
-    tags = {
-        Name = "terraform-example"
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn 
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
+        }
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+    ingress {
+        from_port = 80
+        to _port = 80 
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    egress {
+        from_port = 0
+        to_port
     }
 }
 
@@ -35,6 +85,17 @@ variable "server_port" {
     description = "Port of the server that will handle HTTP requests"
     type = number
     default = 8080
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
 }
 
 output "public_ip" {
